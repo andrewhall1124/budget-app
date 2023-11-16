fc3bf35aca8e77e4e54629976434ef2de07872f8
Andrew Hall <andrewmartinhall2@gmail.com>
2023-11-16 13:59:00 -0700

    trying a js file instead

---
+++ app/manifest.js
@@ -0,0 +1,18 @@
+export default function manifest() {
+  return {
+    name: 'Budget App',
+    short_name: 'Budget App',
+    description: 'Budget App made by Andrew Hall',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    // icons: [
+    //   {
+    //     src: '/favicon.ico',
+    //     sizes: 'any',
+    //     type: 'image/x-icon',
+    //   },
+    // ],
+  }
+}
