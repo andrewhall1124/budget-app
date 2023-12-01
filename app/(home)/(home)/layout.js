65c3368969a3bb616fb1d8cd63536b69a1f3adfe
Andrew Hall <andrewmartinhall2@gmail.com>
2023-11-30 19:59:46 -0700

    Converted auth to getUser service

--- app/(home)/(home)/layout.js
+++ app/(home)/(home)/layout.js
@@ -1,34 +1,18 @@
 'use client'
 import { Footer, Header } from "@/app/components"
 import { useEffect, useState } from "react";
-import { supabase } from "@/app/config/supabaseClient";
 import { useRouter } from "next/navigation";
+import getUser from "@/app/services/getUser";
 
 export default function Layout({ children }) {
-  const [user,setUser] = useState(null)
-  const router = useRouter()
-
-  const getSession = async () =>{
-    try{
-      const {data, error} = await supabase.auth.refreshSession();
-      if(error){
-        console.log("Error:",error)
-        router.push("./login")
-      }
-      else{
-        const {user, session} = data
-        setUser(user)
-        console.log("Success", data)
-      }
-    }
-    catch{
-      console.log("Error:", error)
-    }
-  }
+  const router = useRouter();
+  const user = getUser();
 
   useEffect(() => {
-    getSession();
-  }, []);
+    if(!user){
+      router.push("/login")
+    }
+  }, [user]);
 
 
   return (
